36f876e57b25b272067e317b476101cedcecf7f1
Shea Cronin <shealcronin@gmail.com>
2022-02-02 14:24:41 -0500

    Add form to add new messages and add navbar

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,15 @@ router.get('/', function (req, res, next) {
     res.render('index', { title: 'Mini Message Board', messages });
 });
 
+router.get('/new', (req, res, next) => {
+    res.render('form', { title: 'Mini Message Board' });
+});
+
+router.post('/new', (req, res, next) => {
+    const messageUser = req.body.user;
+    const messageText = req.body.text;
+    messages.push({ user: messageUser, text: messageText, added: new Date() });
+    res.redirect('/');
+});
+
 module.exports = router;
